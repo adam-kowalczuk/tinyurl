83803a94c546e21ea6b4f92c6e5616e6d84caeed
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-20 19:18:23 -0700

    Implemented function that generates a random shortURL

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+function generateRandomString() { //Creates a random 6-character string to be used as a shortURL
+  return (Math.random() + 1).toString(36).substring(6);
+};
 
 app.set("view engine", "ejs"); //Tells Express to use EJS as it's templating engine
 
@@ -9,7 +12,7 @@ const urlDatabase = {
   "9sm5xk": "http://www.google.com"
 };
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //Express library's body parsing middleware to make the POST request body human readable
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -17,11 +20,11 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
+});
 
 // app.get("/set", (req, res) => {
 //   const a = 1;
@@ -35,22 +38,22 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-})
+});
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
-})
+});
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
   res.send("OK");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-})
+});

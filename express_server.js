7b2ad588025f11a0fa9c35b53066f7810bd69a69
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-21 18:14:23 -0700

    Imported cookie-parser and implemented it as middle-ware

--- express_server.js
+++ express_server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const PORT = 8080;
 
@@ -16,6 +18,7 @@ const urlDatabase = {
 //MIDDLEWARE
 
 app.use(express.urlencoded({ extended: true })); //Express library's body parsing middleware to make the POST request body human readable
+app.use(cookieParser);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello!");

83bbd27e314c3f996f8e28ee5173b8fa55f37590
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-26 09:37:11 -0700

    Move databases to separate modules

--- express_server.js
+++ express_server.js
@@ -6,6 +6,8 @@ const app = express();
 const PORT = 8080;
 //Helper functions available to view in helpers.js
 const { generateRandomString, getUserByEmail, getShortURL, getUserByID, urlsForUser } = require('./helpers');
+//"Databases" available to view in database.js
+const { users, urlDatabase } = require('./databases');
 
 //Tells Express to use EJS as it's templating engine
 app.set("view engine", "ejs");
@@ -21,34 +23,6 @@ app.use(cookieSession({
   name: 'session',
   keys: ['secret']
 }));
-
-//"DATABASES"
-
-const urlDatabase = {
-  "b2xVn2": {
-    longURL: "http://www.lighthouselabs.ca",
-    userID: "abc"
-  },
-  "9sm5xk": {
-    longURL: "http://www.google.com",
-    userID: "def"
-  }
-};
-
-const users = {
-  abc: {
-    id: "abc",
-    email: "a@a.com",
-    //FOR EVALUATOR: Plaintext password is '1234'
-    password: "$2a$10$/KNVWX0qJSrHpKQ42vV5Juqv0G9kRaEnYz6lKiJSCtyZnvQUDpEI6",
-  },
-  def: {
-    id: "def",
-    email: "b@b.com",
-    //FOR EVALUATOR: Plaintext password is '5678'
-    password: "$2a$10$8Qxt4EDtL00oRfNfof50uOGxyqsM19HaHWCsutpry1WZDP3Ee69oS",
-  },
-};
 
 //BROWSE
 
--- helpers.js
+++ helpers.js
@@ -1,36 +1,33 @@
-//Creates a random string to be used as a shortURL
+//Create a random string to be used as a shortURL
 const generateRandomString = function() {
   return (Math.random() + 1).toString(36).substring(6);
 };
 
-//Checks to see if ID is registered (used for checking if user is logged in)
+//Check to see if ID is registered (used for checking if user is logged in)
 const getUserByID = function(loggedID, users) {
   for (const user in users) {
     if (users[user].id === loggedID) {
       return users[user];
     }
   }
-  return null;
 };
 
-//Checks whether shortURL exists
+//Check whether shortURL exists
 const getShortURL = function(shortURL, urls) {
   for (const url in urls) {
     if (url === shortURL) {
       return url;
     }
   }
-  return null;
 };
 
-//Checks to see if email is already in use
+//Check to see if email is already in use
 const getUserByEmail = function(email, users) {
   for (const user in users) {
     if (users[user].email === email) {
       return users[user];
     }
   }
-  return null;
 };
 
 //Check for any urls with matching userID, and if so returns an object of those urls
@@ -40,9 +37,6 @@ const urlsForUser = function(id, urls) {
     if (urls[url].userID === id) {
       matchingURLS[url] = urls[url];
     }
-  }
-  if (!Object.keys(matchingURLS).length) {
-    return null;
   }
   return matchingURLS;
 };
